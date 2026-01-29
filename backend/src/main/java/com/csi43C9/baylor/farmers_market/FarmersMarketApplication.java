d5e5935c4daa7ca9c4ba4e5a8b98d25d390ded78
Brendon <brendonnewton0@gmail.com>
2026-01-28 19:30:01 -0600

    Spring initliazr

---
+++ backend/src/main/java/com/csi43C9/baylor/farmers_market/FarmersMarketApplication.java
@@ -0,0 +1,13 @@
+package com.csi43C9.baylor.farmers_market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmersMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmersMarketApplication.class, args);
+	}
+
+}
