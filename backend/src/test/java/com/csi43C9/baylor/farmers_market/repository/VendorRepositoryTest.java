487a1a514aea2c73e85bf333f7c002df976583f9
Brendon <brendonnewton0@gmail.com>
2026-02-13 21:13:00 -0600

    Moving schema setup out of tests

--- backend/src/test/java/com/csi43C9/baylor/farmers_market/repository/VendorRepositoryTest.java
+++ backend/src/test/java/com/csi43C9/baylor/farmers_market/repository/VendorRepositoryTest.java
@@ -33,23 +33,7 @@ class VendorRepositoryTest {
      */
     @BeforeEach
     void setUp() {
-        jdbcTemplate.execute("""
-                create table if not exists vendors (
-                    id binary(16) primary key,
-                    vendor VARCHAR(255) not null,
-                    point_person VARCHAR(255) not null,
-                    email VARCHAR(255) not null,
-                    location VARCHAR(255),
-                    miles INT,
-                    products VARCHAR(255),
-                    is_active boolean default true,
-                    is_farmer boolean default false,
-                    is_produce boolean default false,
-                    woman_owned boolean default false,
-                    bipoc_owned boolean default false,
-                    veteran_owned boolean default false
-                )
-                """);
+        jdbcTemplate.execute("TRUNCATE TABLE vendors");
     }
 
     /**
