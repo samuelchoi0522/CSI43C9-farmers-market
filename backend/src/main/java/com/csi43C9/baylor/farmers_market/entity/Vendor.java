f22398630b4d264c032c412ab892fd860652f001
Brendon <brendonnewton0@gmail.com>
2026-02-16 22:30:19 -0600

    oops this needs a default

--- backend/src/main/java/com/csi43C9/baylor/farmers_market/entity/Vendor.java
+++ backend/src/main/java/com/csi43C9/baylor/farmers_market/entity/Vendor.java
@@ -38,7 +38,7 @@ public class Vendor implements IdentifiableUuid {
     private String products;
 
     /** Whether the vendor is active at the market. */
-    private boolean isActive;
+    private boolean isActive = true;
 
     /** Whether the vendor is a farmer. */
     private boolean isFarmer;
