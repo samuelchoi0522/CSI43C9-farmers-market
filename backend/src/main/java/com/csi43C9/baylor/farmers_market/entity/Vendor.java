c5f173ba4a1b2d47b7e131acb5be3db07440f347
Brendon <brendonnewton0@gmail.com>
2026-02-16 22:35:38 -0600

    Lombok why

--- backend/src/main/java/com/csi43C9/baylor/farmers_market/entity/Vendor.java
+++ backend/src/main/java/com/csi43C9/baylor/farmers_market/entity/Vendor.java
@@ -38,6 +38,7 @@ public class Vendor implements IdentifiableUuid {
     private String products;
 
     /** Whether the vendor is active at the market. */
+    @Builder.Default
     private boolean isActive = true;
 
     /** Whether the vendor is a farmer. */
