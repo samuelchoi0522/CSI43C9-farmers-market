e1a4160475fb366da07f2470a1752261c5c15d92
Jake Mauldin <jake_mauldin1@baylor.edu>
2026-02-22 20:49:48 -0600

    Revert "fixed frontend compilation error"
    This reverts commit 3e7d7e372676bc7eb0c3b5408e190899625a9af4.

--- frontend/app/components/figma/calendar.tsx
+++ frontend/app/components/figma/calendar.tsx
@@ -60,12 +60,12 @@ function Calendar({
         ...classNames,
       }}
       components={{
-      Chevron: ({ className, orientation, ...props }) => {
-        if (orientation === "left") {
-          return <ChevronLeft className={cn("size-4", className)} {...props} />;
-        }
-        return <ChevronRight className={cn("size-4", className)} {...props} />;
-        },
+        IconLeft: ({ className, ...props }) => (
+          <ChevronLeft className={cn("size-4", className)} {...props} />
+        ),
+        IconRight: ({ className, ...props }) => (
+          <ChevronRight className={cn("size-4", className)} {...props} />
+        ),
       }}
       {...props}
     />
