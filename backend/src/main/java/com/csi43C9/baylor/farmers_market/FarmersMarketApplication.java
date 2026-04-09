f0019107fa96125993be139d88acf42d7d652797
Brendon <brendonnewton0@gmail.com>
2026-04-08 22:33:06 -0500

    Updating app to find db file

--- backend/src/main/java/com/csi43C9/baylor/farmers_market/FarmersMarketApplication.java
+++ backend/src/main/java/com/csi43C9/baylor/farmers_market/FarmersMarketApplication.java
@@ -1,6 +1,5 @@
 package com.csi43C9.baylor.farmers_market;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -35,17 +34,18 @@ public class FarmersMarketApplication {
         File appDir;
 
         if (os.contains("win")) {
-            appDir = new File(System.getenv("LOCALAPPDATA"), "FarmersMarketApp");
+            appDir = new File(System.getenv("LOCALAPPDATA"), "MarketOS");
         } else if (os.contains("mac")) {
-            appDir = new File(userHome, "Library/Application Support/FarmersMarketApp");
+            appDir = new File(userHome, "Library/Application Support/MarketOS");
         } else {
-            appDir = new File(userHome, ".farmersmarketapp");
+            appDir = new File(userHome, ".marketos");
         }
 
         if (!appDir.exists()) {
             appDir.mkdirs();
         }
 
+        // This injects a global variable into Spring Boot before it starts
         System.setProperty("app.data.dir", appDir.getAbsolutePath());
     }
 }
